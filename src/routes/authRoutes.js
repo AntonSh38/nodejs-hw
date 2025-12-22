6dfb865acafa689665bb4712dfa654e2e0eaa12d
Anton Shuvalov <anshuvalov83@gmail.com>
2025-12-22 16:16:55 +0200

    add user logout(authController, authRoutes)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,11 +4,16 @@ import {
   loginUserSchema,
   registerUserSchema,
 } from '../validations/authValidation.js';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', celebrate(registerUserSchema), registerUser);
 router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
