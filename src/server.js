a2bc207d2b2bd519895617a5b86ddecdfce72c36
Anton Shuvalov <anshuvalov83@gmail.com>
2025-12-29 12:09:00 +0200

    deleted settings cors

--- src/server.js
+++ src/server.js
@@ -20,12 +20,7 @@ app.use(
     limit: '100kb',
   }),
 );
-app.use(
-  cors({
-    origin: port,
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(cookieParser());
 
 app.use('/auth', authRoutes);
